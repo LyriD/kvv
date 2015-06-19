fdae4d015b1a97fabc421b9ae783e93327ee34e0
Philipp Sheiman <philipp.jwork@gmail.com>
2015-06-19 18:42:06 +0300

    selection

--- AUTOBESTA.RU_files/main.js
+++ AUTOBESTA.RU_files/main.js
@@ -1,14 +1,36 @@
 $(document).ready(function(){
-    $.get('data/cut2.json')
+    $.get('data/or_try_this.json')
         .success(function (data) {
             var html = '';
-            for (var i = 0; i< data.length; i++) {
-                html += '<option value='+i+'>'+data[i].primary+'</option>'
-            }
+            //for (var i = 0; i< data.length; i++) {
+            //
+            //}
+
+            $.each(data, function(key, value) {
+                html += '<option value="'+key+'">'+key+'</option>'
+            });
+
             $('.mark option').after(html);
 
             $('.mark').on('change', function () {
-                console.log($(this).val());
+                $('.model').html('<option value="" disabled selected>Модель</option>')
+                var mark = data[$(this).val()];
+                var options = '';
+                for (var i = 0; i < mark.length; i++) {
+                    options += '<option value="'+i+'">'+mark[i].model+'</option>'
+                }
+                $('.model option').after(options);
+                $('.model').trigger('refresh')
+            });
+
+            $('.red-btn').on('click', function (e) {
+                e.stopPropagation();
+                e.preventDefault();
+
+                var obj = data[$('.mark option:selected').val()][$('.model option:selected').val()];
+
+                $('.img1').find('img').attr('src', obj.f1)
+                $('.img2').find('img').attr('src', obj.f2)
             });
 
             $(' select').styler();
