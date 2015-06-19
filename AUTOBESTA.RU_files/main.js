5c6b483dc6bc95c0b76c6b75c0a63780bf3c45a0
Philipp Sheiman <philipp.jwork@gmail.com>
2015-06-19 18:07:28 +0300

    pre

--- AUTOBESTA.RU_files/main.js
+++ AUTOBESTA.RU_files/main.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
+    $.get('data/cut2.json')
+        .success(function (data) {
+            var html = '';
+            for (var i = 0; i< data.length; i++) {
+                html += '<option value='+i+'>'+data[i].primary+'</option>'
+            }
+            $('.mark option').after(html);
 
-    $(' select').styler();
+            $('.mark').on('change', function () {
+                console.log($(this).val());
+            });
+
+            $(' select').styler();
+        });
+
+
+
 
 });
 
